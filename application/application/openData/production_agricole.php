bd5874d69f54f3d986ea401a90df3ba276e1d1b3
MartinLeBaut <43201651+MartinLeBaut@users.noreply.github.com>
2018-11-14 17:53:15 +0100

    Delete production_agricole.php

--- application/application/openData/production_agricole.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <?php include("header.php"); ?>
-  <body>
-  <iframe src="http://dataportal.opendataforafrica.org/resource/embed/bbkawjf" allowtransparency="true" scrolling="no" frameborder="0" style="height:900px;width:940px;min-width:560px;"></iframe>
-  </body>
-</html>
