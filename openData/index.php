233de8a5f8d2e09595ba96cbda5bbdfd7379e6bd
MartinLeBaut <43201651+MartinLeBaut@users.noreply.github.com>
2018-11-14 17:45:21 +0100

    Add files via upload

---
+++ openData/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <?php include("header.php"); ?>
+  <body>
+    <p align="center"><img src="./welcome.PNG" alt="" height="400px" width="600px" /></p>
+    <p>Pour la réalisation de notre maquette nous avons décidé de montrer l’impact de l’agriculture contre la faim dans le monde.
+      Pour cela nous avons croisé des données concernant l’évolution de la production de l’agriculture en Afrique avec des données concernant l’indice de faim en Afrique.
+      </br>
+      De manière général nous avons remarqué qu’une augmentation de la production de l’agriculture engendre une baisse de l’indice de faim.
+      </br>
+      Au contraire une baisse de la production engendre une augmentation de cet indice.
+      </br>
+      Notre maquette montre que la production agricole et l’indice de faim dans le monde sont corrélés.
+      </br>
+      Ainsi, l’utilisation de l’open data peut permettre de lutter contre la faim dans le monde via l’optimisation des activités agricoles.
+    </p>
+  </body>
+</html>
